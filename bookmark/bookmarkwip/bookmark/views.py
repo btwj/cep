f05b573946172a15d0835114e1973cfaf10b5db8
Bradley Teo <bradleyteowj@gmail.com>
2014-08-11 12:07:19 +0000

    Update Bookmark with User Auth

--- bookmark/bookmarkwip/bookmark/views.py
+++ bookmark/bookmarkwip/bookmark/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 # Import the Category model
 from bookmark.models import Category
 from bookmark.models import Page
-from bookmark.forms import PageForm
+from bookmark.forms import PageForm, UserForm, UserProfileForm
 
 def decode_url(url):
     return url.replace(' ', '_')
@@ -112,4 +114,62 @@ def add_page(request, category_name_url):
         form = PageForm()
         
     return render_to_response('bookmark/add_page.html', {'form': form, 'category': category_name_url}, context)
+
+def register(request):
+    context = RequestContext(request)
+    
+    registered = False
+    
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
         
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            
+            user.set_password(user.password)
+            user.save()
+            
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES('picture')
+            
+            profile.save()
+            registered = True
+        else:
+            print user_form.errors, profile_form.errors
+            
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+        
+    return render_to_response('bookmark/register.html',
+                              {'user_form': user_form, 'profile_form': profile_form, 'registered': registered}, context)
+
+def user_login(request):
+    context = RequestContext(request)
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(username=username, password=password)
+        
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/bookmark/')
+            else:
+                return HttpResponse("Your Bookmark account is disabled")
+        else:
+            print "Invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render_to_response('bookmark/login.html', {}, context)
+    
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/bookmark/')
